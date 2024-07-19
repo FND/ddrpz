0945b248aeb32bbfb219a256c33f2ed5ade67eaa
FND <fnd@localhost.localdomain>
2024-07-19 18:30:00 +0200

    initial commit
    basic infrastructure plus en-/decryption
    
    note that `deno check` references an entry point which doesn't exist yet

---
+++ src/crypto.js
@@ -0,0 +1,55 @@
+/* adapted from <https://prepitaph.org/articles/web-crypto-secrets/> */
+let CRYPTO = globalThis.crypto.subtle;
+let ENC = new TextEncoder();
+let ALGO = "AES-GCM";
+let KEY = {
+	name: "PBKDF2",
+	salt: ENC.encode("7a717688-cc08-4951-ac5b-73828c63eab3").buffer,
+	iterations: 2 ** 20,
+	hash: "SHA-256",
+};
+let IV_SIZE = 16;
+
+/**
+ * @param {string} txt
+ * @param {string} password
+ * @returns {Promise<Uint8Array>}
+ */
+export async function encrypt(txt, password) {
+	let iv = globalThis.crypto.getRandomValues(new Uint8Array(IV_SIZE));
+	let options = { name: ALGO, iv };
+	let res = await CRYPTO.encrypt(options, await deriveKey(password), ENC.encode(txt));
+	let data = new Uint8Array(res);
+	// prepend IV
+	let combo = new Uint8Array(IV_SIZE + data.length);
+	combo.set(iv);
+	combo.set(data, IV_SIZE);
+	return combo;
+}
+
+/**
+ * @param {Uint8Array} data
+ * @param {string} password
+ * @returns {Promise<string>}
+ */
+export async function decrypt({ buffer }, password) {
+	let data = new Uint8Array(buffer, IV_SIZE);
+	let options = {
+		name: ALGO,
+		iv: new Uint8Array(buffer, 0, IV_SIZE),
+	};
+	let res = await CRYPTO.decrypt(options, await deriveKey(password), data);
+	return new TextDecoder().decode(res);
+}
+
+/** @param {string} password */
+async function deriveKey(password) {
+	let secret = await CRYPTO.importKey("raw", ENC.encode(password), KEY.name, false, [
+		"deriveBits",
+		"deriveKey",
+	]);
+	return CRYPTO.deriveKey(KEY, secret, { name: ALGO, length: 256 }, true, [
+		"encrypt",
+		"decrypt",
+	]);
+}
---
+++ src/crypto.test.ts
@@ -0,0 +1,44 @@
+import { decrypt, encrypt } from "./crypto.js";
+import { describe, it } from "$deno/testing/bdd.ts";
+import {
+	assert,
+	assertRejects,
+	assertStrictEquals as assertSame,
+} from "$deno/assert/mod.ts";
+
+let SAMPLE = "lörεm ipsüm dølœr\nßit ämзt";
+let PASSWORD = `s33kr1t ${globalThis.crypto.randomUUID()}`;
+
+describe("encryption", () => {
+	it("turns text into binary data", async () => {
+		let data = await encrypt(SAMPLE, PASSWORD);
+		assert(data instanceof Uint8Array);
+		assertSame(data.length, 66); // XXX: brittle and meaningless?
+		assertSame(await decrypt(data, PASSWORD), SAMPLE); // just to be sure
+	});
+});
+
+describe("decryption", () => {
+	it("turns binary data into text", async () => {
+		let data = await encrypt(SAMPLE, PASSWORD);
+		assertSame(await decrypt(data, PASSWORD), SAMPLE);
+	});
+
+	it("balks at erroneous password", async () => {
+		let data = await encrypt(SAMPLE, PASSWORD);
+		await assertRejects(
+			() => decrypt(data, "…abc123…"),
+			Error,
+			"Decryption failed",
+		);
+	});
+
+	it("balks at non-binary input data", async () => {
+		await assertRejects(
+			// @ts-expect-error TS2345
+			() => decrypt(SAMPLE, PASSWORD),
+			Error,
+			"Tag length overflows ciphertext",
+		);
+	});
+});
